3db3b1a8a7ef519f47c689ff8ccdacab0c971a82
Camila Londoño <camilalondono@192.168.10.125>
2020-11-23 12:12:56 -0500

    copy

--- src/App copy.tsx
+++ src/App copy.tsx
@@ -1,38 +1,4 @@
 import React from 'react';
-import './App.css';
-import Home from './pages/Home';
-import Tensiones from './pages/Tensiones';
-import CrearTension from './pages/CrearTension';
-import Dimensiones from './pages/Dimensiones';
-import Encuestas from './pages/Encuestas';
-import CrearEncuesta from './pages/CrearEncuesta';
-
-import { HashRouter, Switch, Route } from 'react-router-dom';
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-
-import './App.css';
-
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#FF9C00", 
-      /*Naranja */ 
-    },
-    secondary: {
-      main: "#F0EDE7",
-      /*Beige */ 
-    },
-    warning: {
-      main: "#F0EDE7",
-      /*Rosado 2*/ 
-    },
-    success: {
-      main: "#E8853E",
-      /*Naranja 2 */ 
-    },
-  },
-});
 
 
 function AppCopy() {
@@ -40,20 +6,9 @@ function AppCopy() {
 
   return (
 
-    <HashRouter basename={process.env.PUBLIC_URL}>
         <div>
-          <Switch>
-              <ThemeProvider theme={theme}>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/dimensiones' component={Dimensiones} />
-                <Route exact path='/tensiones' component={Tensiones} />
-                <Route exact path='/tensiones/crear/' component={CrearTension} />
-                <Route exact path='/encuestas' component={Encuestas} />
-                <Route exact path='/encuestas/crear/' component={CrearEncuesta} />
-              </ThemeProvider>
-          </Switch>
+          <h1>UNFPA PASE</h1>
         </div>
-      </HashRouter>
   );
 }
 
